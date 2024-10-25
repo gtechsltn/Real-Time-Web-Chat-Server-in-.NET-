b7cb21dd991d0ded7c468ac43cb355698c7aef32
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-10-26 00:44:09 +0600

    hasan2

--- RealTimeWebChat.Api/Program.cs
+++ RealTimeWebChat.Api/Program.cs
@@ -5,10 +5,10 @@ using RealTimeWebChat.Api.Repositorys;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSignalR();
 // Add services to the container.
 builder.Services.AddControllers();
 
-builder.Services.AddSignalR();
 builder.Services.AddScoped<DapperContext>();
 builder.Services.AddScoped<IChatRepository, ChatRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -17,16 +17,19 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Configure CORS policy
+// Configure CORS policy
+// Configure CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins", policy =>
     {
-        policy.WithOrigins("https://localhost:5000") // Add the client URL here
+        policy.WithOrigins("http://localhost:5081") // Add the client URL here
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Allow credentials for SignalR WebSocket connections
     });
 });
+
 
 
 var app = builder.Build();
@@ -37,11 +40,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Enable CORS before routing
 app.UseCors("AllowSpecificOrigins");
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapControllers();
